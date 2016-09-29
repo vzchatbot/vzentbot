8609608a894a5c4f84277ff79212e0ba005452fa
sureshdevaraj <suresh.k.devaraj@gmail.com>
2016-09-29 16:30:27 +0530

    initialcode

---
+++ server.js
@@ -0,0 +1,28 @@
+﻿var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: '31c8d108-cdc4-4474-8e08-c2f6a3b54364',
+    appPassword: 'm5JLXWcRfymrNfHTta454Qj'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
