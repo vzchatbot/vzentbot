1e89f515e3d299abb1871e8c296df373b3629bcb
vzchatbot <vzchatbot@hotmail.com>
2016-10-18 02:12:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,26 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+
+//=============
+
+bot.dialog('/', [
+    function (session) {
+        session.beginDialog('/askName');
+    },
+    function (session, results) {
+        session.send('Hello %s!', results.response);
+    }
+]);
+bot.dialog('/askName', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
+
 //=========================================================
 // Bots Dialogs
 //=========================================================
@@ -49,3 +69,4 @@ bot.dialog('/', function (session)
                                          }); 
     request.end()
 });
+
