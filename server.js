d707d213a1330a4e33eacfe6edee7b54b7b5e17f
vzchatbot <vzchatbot@hotmail.com>
2016-10-18 12:37:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,21 +22,31 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 
-//=============
-    var request = app.textRequest(session.message.text, options);  
-request.on('response', function (response)      
-           {        
-    var intent = response.result.action;      
-    //console.log(JSON.stringify(response)); 
-    session.send(response.result.fulfillment.speech);  
-    var msg = new builder.Message(session).sourceEvent(  
-        {          
-            facebook: response.result.fulfillment.data.facebook.attachment              });   
-    //console.log(JSON.stringify(msg));  
-    session.send(msg);    
-});   
-request.on('error', function (error)          
-           {                console.log(error);  
-           });     request.end()});
-
+//=========================================================
+// Bots Dialogs
+//=========================================================
+bot.dialog('/', function (session)        
+           {    
+    var options =    
+        {     
+            sessionId: '9f04e63b-9ca6-4243-95ef-936be5a94g12'   
+        }   
+    var request = app.textRequest(session.message.text, options);  
+    request.on('response', function (response)       
+               {    
+        var intent = response.result.action;   
+        //console.log(JSON.stringify(response));  
+        session.send(response.result.fulfillment.speech);  
+        var msg = new builder.Message(session).sourceEvent(  
+            {        
+                facebook: response.result.fulfillment.data.facebook.attachment
+            });  
+        //console.log(JSON.stringify(msg));  
+        session.send(msg);   
+    });   
+    request.on('error', function (error)   
+               {    
+        console.log(error);     
+    });  
+    request.end()});
 
