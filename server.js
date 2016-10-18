1b3776347397b85072184a2ce6f8d0cca3242556
vzchatbot <vzchatbot@hotmail.com>
2016-10-18 15:29:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var restify = require('restify');
 var builder = require('botbuilder');
 var apiai = require('apiai');
@@ -26,3 +27,86 @@ server.post('/api/messages', connector.listen());
 // Bots Global Actions
 //=========================================================
 bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+bot.dialog('/', [    function (session)
+                 {   
+                     // Send a greeting and show help.  
+                     /*var card = new builder.HeroCard(session)            
+                     .title("Microsoft Bot Framework")            
+                     .text("Your bots - wherever your users are talking.")            
+                     .images([                
+                     builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")         
+                     ]);  
+                     var msg = new builder.Message(session).attachments([card]); 
+                     session.send(msg);*/      
+                     console.log("hi");       
+                     session.send("Hi... Welcome to the Verizon, How can we help??"); 
+                     session.beginDialog('/startsession');
+                 }
+                 /*,    function (session, results)
+                 {       
+                 // Display menu        
+                 session.beginDialog('/menu'); 
+                 },   
+                 function (session, results) 
+                 {      
+                 // Always say goodbye      
+                 session.send("Ok... See you later!");    }*/
+                ]);
+
+bot.dialog('/menu',
+           [    function (session) 
+            {       
+                builder.Prompts.choice(session, "What demo would you like to run?", "prompts|picture|cards|list|carousel|receipt|actions|(quit)");
+            },  
+            function (session, results) 
+            {       
+                if (results.response && results.response.entity != '(quit)') 
+                {        
+                    // Launch demo dialog       
+                    session.beginDialog('/' + results.response.entity);  
+                } 
+                else
+                {      
+                    // Exit the menu     
+                    session.endDialog(); 
+                }  
+            },  
+            function (session, results)
+            {     
+                // The menu runs a loop until the user chooses to (quit).  
+                session.replaceDialog('/menu'); 
+            }]).reloadAction('reloadMenu', null, { matches: /^menu|show menu/i });
+
+
+bot.dialog('/startsession', [    
+    function (session)  
+                             {
+                                 var options =   
+                                     {
+                                         sessionId: '94642ab5-31b3-4eac-aa1f-d4ef57284007'
+                                     }  
+                                 console.log("inside startsession");
+                                 var request = app.textRequest(session.message.text, options);
+                                 request.on('response', function (response)    
+                                            {            
+                                     var intent = response.result.action; 
+                                     console.log(JSON.stringify(response)); 
+                                     session.send(response.result.fulfillment.speech); 
+                                     var msg = new builder.Message(session).sourceEvent(  
+                                         {                  
+                                             facebook: response.result.fulfillment.data.facebook.attachment  
+                                         });              
+                                     console.log(JSON.stringify(msg));      
+                                     session.send(msg);             
+                                 });                
+                                 request.on('error', function (error)    
+                                            {      
+                                     console.log(error);         
+                                 });             
+                                 request.end();    
+                             }]);
