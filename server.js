4330d7e3ba0066e89a5e475c7d66dc441fbab032
vzchatbot <vzchatbot@hotmail.com>
2016-10-18 12:17:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-﻿var restify = require('restify');
+var restify = require('restify');
 var builder = require('botbuilder');
 var apiai = require('apiai');
-var app = apiai("db847b425ad44ca38e2d696d8b0750cd");
+//var app = apiai("db847b425ad44ca38e2d696d8b0750cd");
 
 //=========================================================
 // Bot Setup
@@ -22,31 +22,41 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 //=========================================================
+// Bots Global Actions
+//=========================================================
+
+bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+
+//=========================================================
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session)
-           { 
-    var options =
-        {
-            sessionId: '94642ab5-31b3-4eac-aa1f-d4ef57284007'
-        } 
-    var request = app.textRequest(session.message.text, options);   
-    request.on('response', function (response) 
-               {        
-        var intent = response.result.action;
-        console.log(JSON.stringify(response));     
-        session.send(response.result.fulfillment.speech);     
-        var msg = new builder.Message(session).sourceEvent(
-            {
-                facebook: response.result.fulfillment.data.facebook.attachment  
-            });
-        console.log(JSON.stringify(msg)); 
-        session.send(msg);   
-    });  
-    request.on('error', function (error)
-               {
-                console.log(error);  
-                                         }); 
-    request.end()
-}); 
+bot.dialog('/', [
+    function (session) {
+        // Send a greeting and show help.
+        var card = new builder.HeroCard(session)
+            .title("Microsoft Bot Framework")
+            .text("Your bots - wherever your users are talking.")
+            .images([
+                 builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")
+            ]);
+        var msg = new builder.Message(session).attachments([card]);
+        session.send(msg);
+        session.send("Hi... I'm the Microsoft Bot Framework demo bot for Facebook. I can show you everything you can use our Bot Builder SDK to do on Facebook.");
+        session.beginDialog('/help');
+    },
+    function (session, results) {
+        // Display menu
+        session.beginDialog('/menu');
+    },
+    function (session, results) {
+        // Always say goodbye
+        session.send("Ok... See you later!");
+    }
+]);
+
+bot.dialog('/help', [
+    function (session) {
+        session.endDialog("Global commands that are available anytime:\n\n* menu - Exits a demo and returns to the menu.\n* goodbye - End this conversation.\n* help - Displays these commands.");
+    }
+]);
