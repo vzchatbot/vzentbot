2ac85970ff8ca2cd517c5dcde3186f77031f1ba6
vzchatbot <vzchatbot@hotmail.com>
2016-10-18 21:32:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ bot.dialog('/getprofile', [
 //=========================================================
 // Bots Dialogs
 //=========================================================
-bot.dialog('/', [    function (session)
+bot.dialog('/receipt', [    function (session)
                  {   
                     // session.beginDialog("/getprofile");                   
                      // Send a greeting and show help.  
@@ -184,3 +184,84 @@ bot.dialog('/menu',
                                  });             
                                  request.end();    
                              }]);
+
+
+//===============
+
+bot.dialog('/', [
+    function (session) {
+        session.send("You can send a receipts for facebook using Bot Builders ReceiptCard...");
+        var msg = new builder.Message(session)
+            .attachments([
+                new builder.ReceiptCard(session)
+                    .title("Recipient's Name")
+                    .items([
+                        builder.ReceiptItem.create(session, "$22.00", "EMP Museum").image(builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/a/a0/Night_Exterior_EMP.jpg")),
+                        builder.ReceiptItem.create(session, "$22.00", "Space Needle").image(builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/7/7c/Seattlenighttimequeenanne.jpg"))
+                    ])
+                    .facts([
+                        builder.Fact.create(session, "1234567898", "Order Number"),
+                        builder.Fact.create(session, "VISA 4076", "Payment Method")
+                    ])
+                    .tax("$4.40")
+                    .total("$48.40")
+            ]);
+        session.send(msg);
+
+        session.send("Or using facebooks native attachment schema...");
+        msg = new builder.Message(session)
+            .sourceEvent({
+                facebook: {
+                    attachment: {
+                        type: "template",
+                        payload: {
+                            template_type: "receipt",
+                            recipient_name: "Stephane Crozatier",
+                            order_number: "12345678902",
+                            currency: "USD",
+                            payment_method: "Visa 2345",        
+                            order_url: "http://petersapparel.parseapp.com/order?order_id=123456",
+                            timestamp: "1428444852", 
+                            elements: [
+                                {
+                                    title: "Classic White T-Shirt",
+                                    subtitle: "100% Soft and Luxurious Cotton",
+                                    quantity: 2,
+                                    price: 50,
+                                    currency: "USD",
+                                    image_url: "http://petersapparel.parseapp.com/img/whiteshirt.png"
+                                },
+                                {
+                                    title: "Classic Gray T-Shirt",
+                                    subtitle: "100% Soft and Luxurious Cotton",
+                                    quantity: 1,
+                                    price: 25,
+                                    currency: "USD",
+                                    image_url: "http://petersapparel.parseapp.com/img/grayshirt.png"
+                                }
+                            ],
+                            address: {
+                                street_1: "1 Hacker Way",
+                                street_2: "",
+                                city: "Menlo Park",
+                                postal_code: "94025",
+                                state: "CA",
+                                country: "US"
+                            },
+                            summary: {
+                                subtotal: 75.00,
+                                shipping_cost: 4.95,
+                                total_tax: 6.19,
+                                total_cost: 56.14
+                            },
+                            adjustments: [
+                                { name: "New Customer Discount", amount: 20 },
+                                { name: "$10 Off Coupon", amount: 10 }
+                            ]
+                        }
+                    }
+                }
+            });
+        session.endDialog(msg);
+    }
+]);
