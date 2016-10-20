4431b81e44058fcbb3dd20493daa1ba1f15653bc
vzchatbot <vzchatbot@hotmail.com>
2016-10-20 16:10:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,59 +21,61 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+// New =============
+// Typing Indicator
+bot.dialog('/countItems', function (session, args) {
+    session.sendTyping();
+    lookupItemsAsync(args, function (err, items) {
+        if (!err) {
+            session.send("%d items found", items.length);
+        } else {
+            session.error(err);
+        }
+    });
+});
 //=========================================================
 // Bots Global Actions
 //=========================================================
+bot.endConversationAction('goodbye', 'Goodbye ,Have a greatday ', { matches: /^goodbye|bye|close/i });
 
-bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+//===========================
+// Get user profile details
+//=======================
 
-//=========================================================
-// Bots Dialogs
-//=========================================================
 
-bot.dialog('/', [
+/* bot.dialog('/', [
     function (session) {
-        
         console.log("=== DIALOG: GETSTARTED | STEP: 1/4 ====");
-
-        //console.log(session.userData);
+        console.log(session.userData);
         // Let the user know we are 'working'
-        //session.sendTyping();
-        //if( !session.userData.firstRun ) {
+        session.sendTyping();
+        if( !session.userData.firstRun ) {
             // Store the returned user page-scoped id (USER_ID) and page id
             session.userData.userid = session.message.sourceEvent.sender.id;
-            session.userData.pageid = session.message.sourceEvent.recipient.id;
-
-            // DELAY ISN'T THE REQUEST - I THINK IT'S THE INITIAL REQUEST TO BOTFRAMEWORK
-
-            // Move to the /getprofile dialog
+            session.userData.pageid = session.message.sourceEvent.recipient.id;       
             session.beginDialog('/getprofile');
-        //} else {
+        } else {
             // The firstname has been stored so the user has completed the /getstarted dialog
             // Stop this dialog and Welcome them back
-          //  session.send('Welcome back');
-        //}
-    }
-]);
-
-// Get users profile
+            session.replaceDialog('/welcomeback');
+        }
+    } */
+//=====================
 bot.dialog('/getprofile', [
     function (session) {
         console.log("=== DIALOG: GETPROFILE | STEP: 1/1 ====");
-        //console.log(session);
+
         // Store the returned user page-scoped id (USER_ID) and page id
         session.userData.userid = session.message.sourceEvent.sender.id;
         session.userData.pageid = session.message.sourceEvent.recipient.id;
-        
-        console.log(session.userData.userid);
-        console.log(session.userData.pageid);
+        console.log( "recipient ID : " + session.message.sourceEvent.recipient.id);
 
         // Let the user know we are 'working'
-        //session.sendTyping();
+        session.sendTyping();
         // Get the users profile information from FB
         request({
-            url: 'https://graph.facebook.com/v2.8/'+ session.userData.userid +'?fields=first_name,last_name,profile_pic,locale,timezone,gender',
-            qs: { access_token: 'EAAZA7BXIxv6IBAF0ce1LuQUZBqepPjBbTnFYcQ9jBITFpFEtoGi3H2kAcBAvT1eTV3BNERepLnpQzexlyIFEmvMrZCBOaROeJgBIlkcGCxwkVtDF92o5ZAvMbBm09ObPxO5opABmcZAZCdD3sp4WwUzh08JU5ZApiQXVBUQWoQhqQZDZD' },
+            url: 'https://graph.facebook.com/v2.6/'+ session.userData.userid +'?fields=first_name,last_name,profile_pic,locale,timezone,gender',
+            qs: { access_token: process.env.EAAYeV8WAScYBAN9UT9yRYZAg4M7jP9zzlnCRVJVfoJ20LAmxR3XgvTo7km6tNBU1HihQn8Bhkhpzh4uT4URydgthctJSfC1nbd0QOHsPtu8YbRdQcQGecZAdVAQ1zkPNrzRQHkP31LBNewmaSx1sAn4NNtnAAWqSWnk1HzvwZDZD  },
             method: 'GET'
         }, function(error, response, body) {
             if (!error && response.statusCode == 200) {
@@ -86,62 +88,106 @@ bot.dialog('/getprofile', [
                 session.dialogData.locale = body.locale;
                 session.dialogData.timezone = body.timezone;
                 session.dialogData.gender = body.gender;
-                //session.dialogData.hometown = body.hometown;
-                //session.dialogData.location = body.location;
                 // Return to /getstarted
-           
-                console.log("Gender " + body.gender);
-                console.log("Locale " + body.locale);
-                console.log("Last Name " + body.last_name);
-                console.log("First Name " + body.first_name);
-                //console.log("Location " + body.hometown);
-                //console.log("Email " + body.email);
                 session.endDialogWithResult({ response: session.dialogData });
             } else {
                 // TODO: Handle errors
                 console.log(error);
                 console.log("Get user profile failed");
+               
             }
         });
     }
 ]);
 
-bot.dialog('/startsession', [
-    function (session)
-           { 
-                var options =
-                    {
-                        sessionId: '94642ab5-31b3-4eac-aa1f-d4ef57284007'
-                    } 
-                  
 
-  
-  //              console.log("inside startsession");
-    //            var request = app.textRequest(session.message.text, options);   
-      //          request.on('response', function (response) 
-        //        {        
-                   /* var intent = response.result.action;
-                    //console.log(JSON.stringify(response));     
-                    session.send(response.result.fulfillment.speech);   
-                    console.log(response.result.fulfillment.data);
-                    console.log(response.result.fulfillment.data.facebook.attachment);
-                    
-                    var msg = new builder.Message(session).sourceEvent(
-                    {
-                        facebook: response.result.fulfillment.data.facebook.attachment
-                    });
-                    //console.log(msg); 
-                    
-                    session.send(msg);   */
-                    
-                    //var actualFBMessage={"attachment":{"type":"template","payload":{"template_type":"generic","elements":[{"title":"Login to Verizon","image_url":"https://www.verizon.com/cs/groups/public/documents/adacct/vzlogo_lg.png","buttons":[{"type":"account_link","url":"https://www98.verizon.com/foryourhome/myaccount/ngen/upr/bots/preauth.html"}]}]}}};
-                    //var datResponse={"speech":"Sign in ","data":{"facebook": actualFBMessage},"contextOut":[{"name":"sigin", "lifespan":2, "parameters":{"type":"signin"}}],"source":"apiaiwebhook"};
-                    //session.send(datResponse);
-          //      });  
-            //    request.on('error', function (error)
-              //  {
-                //   console.log(error);  
-                //}); 
-               // request.end();
-           }
-]);
+//=========================================================
+// Bots Dialogs
+//=========================================================
+bot.dialog('/', [    function (session)
+                 {   
+                  //  console.log( "sender ID : " + session.message.sourceEvent.sender.id);
+                  //   console.log( "recipient ID : " + session.message.sourceEvent.recipient.id);
+                  //  console.log("Page ID" + session.message.sourceEvent.recipient.id);
+                
+                      //console.log("Strat chat");
+                     // Send a greeting and show help.  
+                     var card = new builder.HeroCard(session)            
+                     .title("Verizon Bot ")            
+                     .text("Your bots - wherever your users are talking.")            
+                     .images([                
+                     builder.CardImage.create(session, "http://www.verizon.com//cs/groups/public/documents/adacct/vzlogo_lg.png")         
+                     ]);  
+                     var msg = new builder.Message(session).attachments([card]); 
+                     session.send(msg);     
+                     console.log("hi");                           
+                     session.beginDialog('/startsession');                     
+                 }
+                ,    function (session, results)
+                 {       
+                 // Display menu        
+                 session.beginDialog('/menu');                       
+                 },   
+                 function (session, results) 
+                 {      
+                 // Always say goodbye      
+                 session.send("Ok... See you later!");    }
+                ]);
+
+bot.dialog('/menu',
+           [    function (session) 
+            {       
+                builder.Prompts.choice(session, "What demo would you like to run?", "prompts|picture|cards|list|carousel|receipt|actions|(quit)");
+                console.log( "in menu"+ result.response.entity)
+            },  
+            function (session, results) 
+            {       
+                if (results.response && results.response.entity != '(quit)') 
+                {        
+                    // Launch demo dialog       
+                    session.beginDialog('/' + results.response.entity);  
+                    console.log( "in quit"+ result.response.entity)
+                } 
+                else
+                {      
+                    // Exit the menu     
+                    session.endDialog(); 
+                }  
+            },  
+            function (session, results)
+            {     
+                // The menu runs a loop until the user chooses to (quit).  
+                session.replaceDialog('/menu'); 
+            }]).reloadAction('reloadMenu', null, { matches: /^menu|show menu/i });
+
+
+                bot.dialog('/startsession', [    
+                function (session)  
+                             {
+                                 var options =   
+                                     {
+                                         sessionId: '94642ab5-31b3-4eac-aa1f-d4ef57284007'
+                                     }  
+                                 console.log("inside startsession");
+                                 var request = app.textRequest(session.message.text, options);
+                                 request.on('response', function (response)    
+                                            {            
+                                     var intent = response.result.action; 
+                                    // console.log(JSON.stringify(response)); 
+                                     session.send(response.result.fulfillment.speech); 
+                                     var msg = new builder.Message(session).sourceEvent(  
+                                         {                  
+                                             facebook: response.result.fulfillment.data.facebook.attachment  
+                                         });              
+                                   //  console.log(JSON.stringify(msg));      
+                                     session.send(msg);             
+                                 });                
+                                 request.on('error', function (error)    
+                                            {      
+                                     console.log(error);         
+                                 });             
+                                 request.end();    
+                             }]);
+
+
+//===============
