2e125cc2eb247ee371219862f91366f91e0f18d0
vzchatbot <vzchatbot@hotmail.com>
2016-10-24 12:25:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -120,39 +120,27 @@ bot.dialog('/menu',
 
                 bot.dialog('/startsession', [    
                 function (session)  
-                             {
-                               
-                                 var options =   
+                             { 
+                                var options =
                                      {
-                                         sessionId: '94642ab5-31b3-4eac-aa1f-d4ef57284007'
-                                     }  
-                                 console.log("inside startsession");
-                                 var request = app.textRequest(session.message.text, options);
-                                 var text1= response.result.fulfillment.data.facebook.attachment.payload.text;
-                                 
-                                      if (text1 == "" || text1 == undefined) {
-                                           request.on('response', function (response)    
-                                            {            
-                                     var intent = response.result.action; 
+                                          sessionId: '94642ab5-31b3-4eac-aa1f-d4ef57284007'
+                                        } 
+                                  console.log("inside startsession");
+                                   var request = app.textRequest(session.message.text, options);
+                                   //  console.log(result.fulfilment.data);
+                                   request.on('response', function (response)
+                                            { 
+                                     var intent = response.result.action;
                                      console.log(JSON.stringify(response));
-                                     console.log("In SPEECH ");  
-                                     console.log('Text1 value'+ JSON.stringify(text1));
+			                         console.log(" Attachment :" + JSON.stringify(response.result.fulfillment.data.facebook.attachment.payload.buttons));
                                      session.send(response.result.fulfillment.speech); 
-                                        var msg = new builder.Message(session).sourceEvent(
-                                         facebook: response.result.fulfillment.data.facebook.attachment.payload );  //for speech
-                                     session.send(msg); 
-                                 });                                  
-                                         else 
-                                              request.on('response', function (response)    
-                                            {            
-                                     var intent = response.result.action; 
-                                     console.log(JSON.stringify(response));
-                                     console.log("In TEXT ");
-                                     console.log('Text1 value'+ JSON.stringify(text1));
-                                     session.send(response.result.fulfillment.data.facebook.attachment); 
-                                      var msg = new builder.Message(session).sourceEvent(                                                                                                                                
-                                      facebook: response.result.fulfillment.data.facebook ); // for text 
-                                     session.send(msg); 
+                                     var msg = new builder.Message(session).sourceEvent( 
+                                         {              
+                                             facebook: response.result.fulfillment.data.facebook
+                                           // facebook: response.result.fulfillment.data.facebook.attachment.payload.buttons 
+                                        });  
+   // console.log(JSON.stringify(msg));
+                                     session.send(msg);
                                  });                                          
                                          }                                             
                                  request.on('error', function (error)    
