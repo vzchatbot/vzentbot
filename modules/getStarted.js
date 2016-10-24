12ab775a94637a6a03a226dc0a40dce2d1849923
vzchatbot <vzchatbot@hotmail.com>
2016-10-24 22:46:22 +0530

    Update getStarted.js

--- modules/getStarted.js
+++ modules/getStarted.js
@@ -4,12 +4,13 @@ var getStarted = function () { };
 getStarted.prototype.dogetStarted = function (session, response, builder) {    
                              
                                      console.log("inside startsession");                                   
-                                         session.send(response.result.fulfillment.speech);
+                                         session.send(response.result.fulfillment.displayText);
                                          var msg = new builder.Message(session).sourceEvent(
                                              {
                                                  facebook: response.result.fulfillment.data.facebook
+                                             
                                                  // facebook: response.result.fulfillment.data.facebook.attachment.payload.buttons 
                                              });  
-
+session.send(msg);
 }
 exports.getStarted = new getStarted();
