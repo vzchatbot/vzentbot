83a3057945afbbea12db8347d748ca77fe3827f6
vzchatbot <vzchatbot@hotmail.com>
2016-10-24 14:46:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -134,12 +134,15 @@ bot.dialog('/menu',
                                      
                                      var text1= response.result.fulfillment.data.facebook.attachment.payload.text;
                                  
-                                     if (text1 == "" || text1 == undefined) {
+                                     if (text1 == "" || text1 == undefined)
+                                     {
                                      session.send(response.result.fulfillment.speech);
                                      var msg1 = new builder.Message(session).sourceEvent(
-                                     facebook: response.result.fulfillment.data.facebook.attachment.payload );  
+                                         {
+                                     facebook: response.result.fulfillment.data.facebook.attachment.payload 
+                                         });  
                                      session.send(msg1); 
-                                      }
+                                     }
                                      else
                                      session.send(response.result.fulfillment.text);
                                      var msg = new builder.Message(session).sourceEvent(  
