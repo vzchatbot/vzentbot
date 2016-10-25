0e00679beab1303a8f5ece28f21c529171a34b3c
vzchatbot <vzchatbot@hotmail.com>
2016-10-25 15:43:06 +0530

    Update record.js

--- modules/record.js
+++ modules/record.js
@@ -2,6 +2,36 @@
 var Record = function () { };
 
 Record.prototype.doRecord = function (req, res) {
+
+    return (
+        {
+            speech: "Hey Tabi, welcome to Verizon! Want to know what’s on tonight?  I can answer almost anything, so try me! Also, if you want personalized alerts through Messenger link me to your Verizon account! ",
+            displayText: "Link Account",
+            data: {
+                "facebook": {
+                    "attachment": {
+                        "type": "template",
+                        "payload": {
+                            "template_type": "button",
+                            "text": "Hey , welcome to Verizon! Want to know what’s on tonight?  I can answer almost anything, so try me! Also, if you want personalized alerts through Messenger link me to your Verizon account! ",
+                            "buttons": [
+                                {
+                                    "type": "postback",
+                                    "title": "Link Account",
+                                    "payload": "Link Account"
+                                }
+                            ]
+                        }
+                    }
+                }
+            },
+            source: "Verizon.js"
+        }
+
+}
+
+/*
+Record.prototype.doRecord = function (req, res) {
     var channel = req.body.result.parameters.Channel.toUpperCase();
     var program = req.body.result.parameters.Programs.toUpperCase();
     var time = req.body.result.parameters.timeofpgm;
@@ -110,5 +140,5 @@ Record.prototype.doRecord = function (req, res) {
 
     break;
 };
-
+*/
 exports.Record = new Record();
