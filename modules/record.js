111ad0b3f00fbde6be85b8a0b3d8262a94ffd11a
vzchatbot <vzchatbot@hotmail.com>
2016-10-25 18:23:53 +0530

    Update record.js

--- modules/record.js
+++ modules/record.js
@@ -5,27 +5,13 @@ Record.prototype.doRecord = function (session, response, builder) {
                                          console.log("----------record.js:SELECTED INTENT IS recordnew----------");
                                          console.log("inside startsession");                                   
                                          session.send(response.result.fulfillment.source);
-                                         console.log(response.result.contexts.name);
-                                         /*var msg = new builder.Message(session).sourceEvent(
-                                             {
-                                               "facebook": {
-                                                "attachment": {
-                                                    "type": "template",
-                                                    "payload": {
-                                                        "template_type": "button",
-                                                        "text": "Hey , welcome to Verizon! Want to know what’s on tonight?  I can answer almost anything, so try me! Also, if you want personalized alerts through Messenger link me to your Verizon account! ",
-                                                        "buttons": [
-                                                            {
-                                                                "type": "postback",
-                                                                "title": "Link Account",
-                                                                "payload": "Link Account"
-                                                            }
-                                                        ]
-                                                    }
-                                                }
-                                            }
-                                             });  
-                                    session.send(msg);
-                                    */
-}
+                                         console.log(response.result.parameters);
+                                         var channel =response.result.parameters.Channel.toUpperCase();
+                                         var program = response.result.parameters.Programs.toUpperCase();
+                                         var time = response.result.parameters.timeofpgm;
+                                         var dateofrecord = response.result.parameters.date;
+                                         var SelectedSTB = response.result.parameters.SelectedSTB;
+                                        console.log(channel ,program,time,dateofrecord,SelectedSTB);
+  
+                                    
 exports.Record = new Record();
