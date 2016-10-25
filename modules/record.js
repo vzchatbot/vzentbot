f4b62db9dd944c792e71c17875f7eeed11cbc54c
vzchatbot <vzchatbot@hotmail.com>
2016-10-25 15:52:25 +0530

    Update record.js

--- modules/record.js
+++ modules/record.js
@@ -5,27 +5,9 @@ Record.prototype.doRecord = function (req, res) {
 
     return (
         {
-            speech: "Hey Tabi, welcome to Verizon! Want to know what’s on tonight?  I can answer almost anything, so try me! Also, if you want personalized alerts through Messenger link me to your Verizon account! ",
-            displayText: "Link Account",
-            data: {
-                "facebook": {
-                    "attachment": {
-                        "type": "template",
-                        "payload": {
-                            "template_type": "button",
-                            "text": "Hey , welcome to Verizon! Want to know what’s on tonight?  I can answer almost anything, so try me! Also, if you want personalized alerts through Messenger link me to your Verizon account! ",
-                            "buttons": [
-                                {
-                                    "type": "postback",
-                                    "title": "Link Account",
-                                    "payload": "Link Account"
-                                }
-                            ]
-                        }
-                    }
-                }
-            },
-            source: "Verizon.js"
+            console.log("inside startsession");                                   
+            session.send(response.result.fulfillment.displayText);
+          
         }
 
 }
