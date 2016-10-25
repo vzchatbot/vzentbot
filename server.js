4266757b947085324faaf4667e16fb94b5d6d090
vzchatbot <vzchatbot@hotmail.com>
2016-10-25 17:41:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -122,9 +122,19 @@ bot.dialog('/startsession', [
                     sessionId: '94642ab5-31b3-4eac-aa1f-d4ef57284007'
                 } 
 		console.log("Full Session ", session);
-		console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking,null,2)); 
-		console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking.authorization_code,null,2)); 
-		console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking.status,null,2)); 
+		
+		if(session.message.sourceEvent.account_linking == undefined)
+		   {
+			 console.log("Account Linking null");
+			
+		   }
+		   else
+		   {
+			   console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking,null,2)); 
+			console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking.authorization_code,null,2)); 
+			console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking.status,null,2)); 
+			   
+		   }
 		console.log("Start getting information from API.AI after profile call");
                 console.log("Message Text "+ session.message.text);
                 var request = app.textRequest(session.message.text, options);   
@@ -150,115 +160,8 @@ bot.dialog('/startsession', [
                     console.log(error);  
                 }); 
 
-               // session.replaceDialog('/accountlink');
+                session.endDialog();
                 request.end();
            
            } // end of function declaration
 ]); // End of dialoag function
-
-bot.dialog('/accountlink', [
-    function (session) {
-        console.log("Account Linking function");
-        
-        var msg = new builder.Message(session)
-
-            .sourceEvent({
-
-                facebook: {
-
-                    attachment: {
-
-                        type: "template",
-
-                        payload: {
-
-                            template_type: "button",
-
-                            text: "Welcome, Link your Verizon Account",
-                            
-                            buttons:[{
-                                    type: "account_link",
-                                    url: "https://www98.verizon.com/foryourhome/myaccount/ngen/upr/bots/preauth.aspx"
-                            }],
-
-                        } //Payload
-
-                    } // attachment
-
-                } // facebook
-
-            }); // sourcevent
-
-        session.send(msg);
-        session.endDialog();
-           } // end of function declaration
-]); // End of dialoag function account link
-        
-function messageEvents(req, res){
-     console.log("MessegeEvent Function");
-   var data = req.body;
-  // Make sure this is a page subscription
-  
-  if (data.object == 'page') {
-  
-    // Iterate over each entry
-    // There may be multiple if batched
-
-    data.entry.forEach(function(pageEntry) {
-      var pageID = pageEntry.id;
-      var timeOfEvent = pageEntry.time;
-
-      // Iterate over each messaging event
-
-      pageEntry.messaging.forEach(function(messagingEvent) {
-
-        if (messagingEvent.optin) {
-          receivedAuthentication(messagingEvent);
-	      console.log("Webhook received following messagingEvent optin");
-        } else if (messagingEvent.message) {
-          //receivedMessage(messagingEvent);
-	      console.log("Webhook received following messagingEvent message");
-        } else if (messagingEvent.delivery) {
-          //receivedDeliveryConfirmation(messagingEvent);
-	       console.log("Webhook received following messagingEvent delivery");
-        } else if (messagingEvent.postback) {
-          //receivedPostback(messagingEvent);
-	      console.log("Webhook received following messagingEvent postback");
-        } else {
-          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-        }
-
-      });
-    });
-
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know you've 
-    // successfully received the callback. Otherwise, the request will time out.
-
-    res.sendStatus(200);
-
-  } // if loop
-} //close of function
-
-
-function receivedAuthentication(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfAuth = event.timestamp;
-
-  // The 'ref' field is set in the 'Send to Messenger' plugin, in the 'data-ref'
-  // The developer can set this to an arbitrary value to associate the 
-  // authentication callback with the 'Send to Messenger' click event. This is
-  // a way to do account linking when the user clicks the 'Send to Messenger' 
-  // plugin.
-  var passThroughParam = event.optin.ref;
-
-  console.log("Received authentication for user %d and page %d with pass " +
-    "through param '%s' at %d", senderID, recipientID, passThroughParam, 
-    timeOfAuth);
-
-  // When an authentication is received, we'll send a message back to the sender
-  // to let them know it was successful.
-  console.log("FB and Verizon Link successful");
-}
