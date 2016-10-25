7e6d6f024712856565f6439c4d36d0f7356b02eb
vzchatbot <vzchatbot@hotmail.com>
2016-10-25 18:35:08 +0530

    Update record.js

--- modules/record.js
+++ modules/record.js
@@ -6,12 +6,13 @@ Record.prototype.doRecord = function (session, response, builder) {
                                          console.log("inside startsession");                                   
                                          session.send(response.result.fulfillment.source);
                                          console.log(response.result.parameters);
-                                         var channel =(response.result.parameters.Channel);
+                                      /*   var channel =(response.result.parameters.Channel);
                                          var program = (response.result.parameters.Programs);
                                          var time = (response.result.parameters.timeofpgm);
                                          var dateofrecord = (response.result.parameters.date);
                                          var SelectedSTB = (response.result.parameters.SelectedSTB);
 console.log("SelectedSTB : " + SelectedSTB + " channel : " + channel + " dateofrecord :" + dateofrecord + " time :" + time);
+*/
   
                                     
 exports.Record = new Record();
