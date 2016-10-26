cc5a988134844402d87e03942bd3245b8d7a7465
vzchatbot <vzchatbot@hotmail.com>
2016-10-26 15:10:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,167 +1,124 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var apiai = require('apiai');
-//var app = apiai("901c05fa26b7415196db699acdc5d193"); // Prabu
-var app = apiai("db847b425ad44ca38e2d696d8b0750cd"); // Mine
-var request = require('request');
+var nconf = require('nconf');
+var uuid = require('node-uuid');
+
+nconf.file('./config/config.json');
+var app = apiai(nconf.get('apiai:clientid'));
 
 //=========================================================
 // Bot Setup
 //=========================================================
 
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
+    console.log('NODE_ENV: ' + nconf.get('NODE_ENV'));
+    console.log('Microsoft_AppID: ' + nconf.get('msbot:Microsoft_AppID'));
+    console.log('apiai: ' + nconf.get('apiai:clientid'));
 });
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: '4f5df286-2591-477d-af7d-547dd13a0156',
-    appPassword: 'oTwfDnKyx1gCwiQoP53k2oC'
+    appId: nconf.get('msbot:Microsoft_AppID'),
+    appPassword: nconf.get('msbot:Microsoft_AppPassword')
 });
-
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 //=========================================================
-// Bots Global Actions
-//=========================================================
-
-//bot.beginDialog('/startsession');
-bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
-
-//=========================================================
 // Bots Dialogs
 //=========================================================
-server.get('/test', function (req, res) {
-	console.log("botwebook ");
-});
-	    
-bot.dialog('/', [
-    function (session) {
-        
-        console.log("=== DIALOG: New Session Started ====");
-        console.log("Session Info " + session.message);
-        
-        if( !session.userData.firstRun ) {
-            // Store the returned user page-scoped id (USER_ID) and page id
-            session.userData.userid = session.message.sourceEvent.sender.id;
-            session.userData.pageid = session.message.sourceEvent.recipient.id;
 
-            // DELAY ISN'T THE REQUEST - I THINK IT'S THE INITIAL REQUEST TO BOTFRAMEWORK
-            // Move to the /getprofile dialog
-            //session.beginDialog('/getprofile');
-            session.beginDialog('/startsession');
-            //messageEvents(req,res);
-        } else {
-            // The firstname has been stored so the user has completed the /getstarted dialog
-            // Stop this dialog and Welcome them back
-            console.log("Welcome Back User");
-            console.log("Gender " + session.dialogData.gender);
-            console.log("Locale " + session.dialogData.locale);
-            console.log("Last Name " + session.dialogData.lastname);
-            console.log("First Name " + session.dialogData.firstname);
-            session.send('Welcome back');
+bot.dialog('/', function (session) {
+    var options = { sessionId: '94642ab5-31b3-4eac-aa1f-d4ef57284007' }
+
+    var request = app.textRequest(session.message.text, options);
+    request.on('response', function (response) {
+        var intent = response.result.action;
+        console.log(JSON.stringify(response));
+        var Finished_Status = response.result.fulfillment.speech
+        console.log("Finished_Status " + Finished_Status);
+        if (Finished_Status !== "IntentFinished") {
+            session.send(response.result.fulfillment.speech);
+            console.log("-----------INTENT NOT YET FINISHED-----------");
         }
-    }
-]);
+        else if (Finished_Status == "IntentFinished") {
+            console.log("-----------INTENT SELECTION-----------");
+            var Selected_intentName = response.result.fulfillment.source
+            console.log("Selected_intentName : " + Selected_intentName);
 
-// Get facebook users profile
-bot.dialog('/getprofile', [
-    function (session) {
-        console.log("=== DIALOG: GETPROFILE | STEP: 1/1 ====");
-        //console.log(session);
-        // Store the returned user page-scoped id (USER_ID) and page id
-        session.userData.userid = session.message.sourceEvent.sender.id;
-        session.userData.pageid = session.message.sourceEvent.recipient.id;
-        
-        console.log("FB User ID " + session.userData.userid);
-        console.log("FB Page ID " + session.userData.pageid);
+            switch (Selected_intentName) {
 
-        // Let the user know we are 'working'
-        //session.sendTyping();
-        // Get the users profile information from FB
-        request({
-            url: 'https://graph.facebook.com/v2.8/'+ session.userData.userid +'?fields=first_name,last_name,profile_pic,locale,timezone,gender',
-            qs: { access_token: 'EAAZA7BXIxv6IBAF0ce1LuQUZBqepPjBbTnFYcQ9jBITFpFEtoGi3H2kAcBAvT1eTV3BNERepLnpQzexlyIFEmvMrZCBOaROeJgBIlkcGCxwkVtDF92o5ZAvMbBm09ObPxO5opABmcZAZCdD3sp4WwUzh08JU5ZApiQXVBUQWoQhqQZDZD' },
-            method: 'GET'
-        }, function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                // Parse the JSON returned from FB
-                body = JSON.parse(body);
-                // Save profile to userData
-                session.dialogData.firstname = body.first_name;
-                session.dialogData.lastname = body.last_name;
-                session.dialogData.profilepic = body.profile_pic;
-                session.dialogData.locale = body.locale;
-                session.dialogData.timezone = body.timezone;
-                session.dialogData.gender = body.gender;
-                
-                console.log("Last Name " + body.last_name);
-                console.log("First Name " + body.first_name);           
-                console.log("Gender " + body.gender);
-                console.log("Locale " + body.locale);
-                
-                // Return to /startSession
-                session.endDialogWithResult({ response: session.dialogData });
-            } else {
-                // TODO: Handle errors
-                console.log(error);
-                console.log("Get user profile failed");
+                case "welcome":
+                    var welcome = require('./modules/welcome.js').Welcome;
+                    break;
+                case "recordnew":
+                    var Record = require('./modules/record.js').Record;
+                    Record.doRecord(session, response, builder);
+
+                    /*  case "recordnew":
+                      var Record = require('./modules/record.js').Record;
+                      Record.doRecord(session, response, builder);				    
+                      break; */
+
+                case "LinkOptions":
+
+
+                    break;
+                case "MoreOptions":
+                    var moreOptions = require('./modules/MoreOptions.js').MoreOptions;
+                    break;
+                case "Billing":
+                    var billing = require('./modules/MoreOptions.js').Billing;
+                    break;
+                case "stblist":
+
+                    break;
+                case "upsell":
+
+                    break;
+                case "upgradeDVR":
+
+                    break;
+                case "stgexternalcall":
+
+                    break;
+                case "Trending":
+
+                    break;
+                case "recommendation":
+
+                    break;
+                case "channelsearch":
+
+                    break;
+                case "programSearchdummy":
+
+                    break;
+                case "programSearch":
+
+                    break;
+                case "getStarted":
+
+                    getStarted.dogetStarted(req, res);
+                default:
+
             }
-        });
-    }
-]);
+        }
 
-bot.dialog('/startsession', [
-    function (session)
-           { 
-                var options =
-                {
-                    sessionId: '94642ab5-31b3-4eac-aa1f-d4ef57284007'
-                } 
-		console.log("Full Session ", session);
-		
-		if(session.message.sourceEvent.account_linking == undefined)
-		   {
-			 console.log("Account Linking null");
-			
-		   }
-		   else
-		   {
-			   console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking,null,2)); 
-			console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking.authorization_code,null,2)); 
-			console.log("Account Linking convert: " + JSON.stringify(session.message.sourceEvent.account_linking.status,null,2)); 
-			   
-		   }
-		console.log("Start getting information from API.AI after profile call");
-                console.log("Message Text "+ session.message.text);
-                var request = app.textRequest(session.message.text, options);   
-               
-                request.on('response', function (response) 
-                {        
-                    var intent = response.result.action;
-                    console.log("Action " + intent);     
-                    //session.send(response.result.fulfillment.speech);  
-                    //console.log(" TEXT1 :" + JSON.stringify(response.result.fulfillment.data.facebook.attachment.payload.text));
-	            console.log(" Attachment value :" + JSON.stringify(response.result.fulfillment.data)); 
-                   
-		    var msg = new builder.Message(session).sourceEvent(
-                    {
-                        facebook: response.result.fulfillment.data.facebook
-                    });
-		   
-		   session.send(msg);	
-			
-                });  
-                request.on('error', function (error)
-                {
-                    console.log(error);  
-                }); 
+        //var msg = new builder.Message(session).attachment(response.result.fulfillment.data.facebook.attachment);
+        //console.log(JSON.stringify(msg));
+        //session.send(msg);
+    });
+    request.on('error', function (error) {
+        console.log(error);
+    });
 
-                session.endDialog();
-                request.end();
-           
-           } // end of function declaration
-]); // End of dialoag function
+    request.end()
+
+
+});
