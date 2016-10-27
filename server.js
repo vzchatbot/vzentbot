73637e575d0cbfde60583fc65689fe90a5f633ff
vzchatbot <vzchatbot@hotmail.com>
2016-10-27 13:14:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ server.post('/api/messages', connector.listen());
 
 bot.dialog('/', function (session) {
     var options = {sessionId: '9dbb0570-9a8b-11e6-a1e7-afc8eaec72d6'}
-    session.beginDialog('/getprofile');
+   // session.beginDialog('/getprofile');
 	
     var request = app.textRequest(session.message.text, options);
     request.on('response', function (response) {
