32701005701bd0119154179ed98de0c227076794
vzchatbot <vzchatbot@hotmail.com>
2016-10-27 12:32:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -111,6 +111,7 @@ function welcomeMsg(usersession)
      console.log("inside welcomeMsg");
        var respobj= {"facebook":{"attachment":{"type":"template","payload":{"template_type":"button","text":"Want to know what’s on tonight? When your favorite sports team is playing? What time your favorite show is coming on? I can answer almost anything, so try me! Before we get started—let’s take a few minutes to get me linked to your Verizon account, this way I can send you personalized recommendations, alerts.","buttons":[{"type":"postback","title":"Link Account","payload":"Link Account"},{"type":"postback","title":"Maybe later","payload":"Main Menu"}]}}}};
 	 console.log(JSON.stringify(respobj)); 
+	usersession.send("Hi Welcome to Verizon");
 	var msg = new builder.Message(usersession).sourceEvent(respobj);              
           usersession.send(msg);
 }
