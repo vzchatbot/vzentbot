30a87ea7a265101c5e81f0b8d53d93c7d72a4255
vzchatbot <vzchatbot@hotmail.com>
2016-10-28 19:18:36 +0530

    Update userslog.js

--- userslog.js
+++ userslog.js
@@ -3,11 +3,11 @@ var winston = require('winston');
 var logger = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)({ json: false, timestamp: true }),
-    new winston.transports.File({ filename: './Customlog' + '/debug.log', json: false })
+    new winston.transports.File({ filename: './debug.log', json: false })
   ],
   exceptionHandlers: [
     new (winston.transports.Console)({ json: false, timestamp: true }),
-    new winston.transports.File({ filename: './Customlog' + '/exceptions.log', json: false })
+    new winston.transports.File({ filename:  './exceptions.log', json: false })
   ],
   exitOnError: false
 });
