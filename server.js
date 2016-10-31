35c1c202f80f16f3282b335bc31c25d586102140
vzchatbot <vzchatbot@hotmail.com>
2016-10-31 19:33:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,23 +10,6 @@ var app = apiai(nconf.get('apiai:clientid'));
 
 
 //===================================================
-if (process.argv.length <= 2) {
-  //  console.log("Usage: " +./vzentbot + " path/to/directory");
-    process.exit(-1);
-} 
-var path = process.argv[2];
- 
-fs.readdir(path, function(err, items) {
-    for (var i=0; i<items.length; i++) {
-        var file = path + '/' + items[i];
-        console.log("Start: " + file);
- 
-        fs.stat(file, function(err, stats) {
-            console.log(file);
-            console.log(stats["size"]);
-        });
-    }
-});
 
 //===================================================
 
