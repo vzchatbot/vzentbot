5c237a38b0686cbe36960e474311b68f2bf898c5
vzchatbot <vzchatbot@hotmail.com>
2016-11-18 19:27:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -686,8 +686,9 @@ function PgmSearchCallback(apiresp,usersession) {
         && subflow.facebook != null 
         && subflow.facebook.text != null && subflow.facebook.text =='UserNotFound')
 	{
-		subflow ='{"facebook":{"attachment":{"type":"template","payload":{"template_type":"generic","elements":[{"title":"You have to Login to Verizon to proceed","image_url":"https://www98.verizon.com/foryourhome/vzrepair/siwizard/img/verizon-logo-200.png","buttons":{"type":"account_link","url":"https://www98.verizon.com/vzssobot/upr/preauth"}}]}}}}';
-		var msg = new builder.Message(usersession).sourceEvent(subflow);              
+		console.log (subflow.facebook.text);
+		var respobj ='{"facebook":{"attachment":{"type":"template","payload":{"template_type":"generic","elements":[{"title":"You have to Login to Verizon to proceed","image_url":"https://www98.verizon.com/foryourhome/vzrepair/siwizard/img/verizon-logo-200.png","buttons":{"type":"account_link","url":"https://www98.verizon.com/vzssobot/upr/preauth"}}]}}}}';
+		var msg = new builder.Message(usersession).sourceEvent(respobj);              
         	usersession.send(msg);
 	}
 	else
